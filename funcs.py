c7e8a34f0e56a1f709445d1af438ae842ddeab46
Кристина <christinavinisuelo@gmail.com>
2024-10-25 12:13:01 +0300

    тест версификации

---
+++ funcs.py
@@ -0,0 +1,21 @@
+def func1():
+    return 'первый'
+
+
+def func2():
+    return func1() + ' и второй'
+
+
+def func3():
+    return func2() + ' и третий'
+
+
+def func4():
+    return func3() + ' и четвёртый
+    
+    
+  def func5():
+    return func4() + ' и пятый'    
+    
+    
+    
